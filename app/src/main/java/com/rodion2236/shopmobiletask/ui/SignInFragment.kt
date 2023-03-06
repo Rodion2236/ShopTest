b33ac09d0b91a9823c8110d1451a39ce39e3d35f
Rodion <kosolapkinr@gmail.com>
2023-03-06 13:02:50 +0400

    add dependencies

--- app/src/main/java/com/rodion2236/shopmobiletask/ui/SignInFragment.kt
+++ app/src/main/java/com/rodion2236/shopmobiletask/ui/SignInFragment.kt
@@ -5,14 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.rodion2236.shopmobiletask.R
+import com.rodion2236.shopmobiletask.databinding.FragmentSignInBinding
 
 class SignInFragment : Fragment() {
+
+    private var _fragmentSignInBinding: FragmentSignInBinding? = null
+    private val fragmentSignInBinding = checkNotNull(_fragmentSignInBinding)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    ): View {
+        _fragmentSignInBinding = FragmentSignInBinding.inflate(inflater, container, false)
+        return fragmentSignInBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fragmentSignInBinding.button.setOnClickListener {
+
+        }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        _fragmentSignInBinding = null
     }
 }
+//проверить валидность емайл, чистка после добавки, добавление в базу, проверка всех полей на пустоту

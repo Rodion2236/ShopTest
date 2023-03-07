d1de1ac4fd52c996fb9bd0c2868e3c43651acedb
Rodion <kosolapkinr@gmail.com>
2023-03-07 15:19:23 +0400

    add icons

---
+++ app/src/main/java/com/rodion2236/shopmobiletask/model/Icons.kt
@@ -0,0 +1,14 @@
+package com.rodion2236.shopmobiletask.model
+
+import com.rodion2236.shopmobiletask.R
+
+enum class Icons(val icons: Int) {
+    //телефон/наушники/игры/авто/фурнитура/дети
+
+    Phones(R.drawable.phone),
+    Headphones(R.drawable.headphones),
+    Games(R.drawable.games),
+    Cars(R.drawable.auto),
+    Furniture(R.drawable.furnitura),
+    Kids(R.drawable.kids)
+}

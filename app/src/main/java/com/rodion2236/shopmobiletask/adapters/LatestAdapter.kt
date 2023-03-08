5fd084595ed6a832c0ddc111b42d36f7a19e3d41
Rodion <kosolapkinr@gmail.com>
2023-03-08 15:44:11 +0400

    create latest adapter

--- app/src/main/java/com/rodion2236/shopmobiletask/adapters/LatestAdapter.kt
+++ app/src/main/java/com/rodion2236/shopmobiletask/adapters/LatestAdapter.kt
@@ -1,29 +1,46 @@
 package com.rodion2236.shopmobiletask.adapters
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.rodion2236.shopmobiletask.databinding.ListLatestBinding
 import com.rodion2236.shopmobiletask.model.latest.InfoLatest
 import com.rodion2236.shopmobiletask.model.latest.Latest
 
-class LatestAdapter(
-    private var latest: List<InfoLatest>
-    ): RecyclerView.Adapter<LatestAdapter.ViewHolder>() {
+class LatestAdapter(): RecyclerView.Adapter<LatestAdapter.ViewHolder>() {
 
-    class ViewHolder {
+    inner class ViewHolder(
+        private val latestBinding: ListLatestBinding
+    ): RecyclerView.ViewHolder(latestBinding.root) {
 
+        fun bind(infoLatest: InfoLatest) {
+            Glide.with(latestBinding.root)
+                .load(infoLatest.image_url)
+                .centerCrop()
+                .into(latestBinding.ivConsole)
+
+            latestBinding.tvConsole.text = infoLatest.category
+            latestBinding.tvNameConsole.text = infoLatest.name
+            latestBinding.tvCost.text = infoLatest.price.toString()
+        }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LatestAdapter {
-        val binding = Late
+    private var listLatest = mutableListOf<InfoLatest>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LatestAdapter.ViewHolder {
+        val binding = ListLatestBinding
+            .inflate(LayoutInflater
+            .from(parent.context), parent, false)
+        return ViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
-        return latest.size
+        return listLatest.size
     }
 
-    override fun onBindViewHolder(holder: LatestAdapter, position: Int) {
-        holder.bind(latest[position])
+    override fun onBindViewHolder(holder: LatestAdapter.ViewHolder, position: Int) {
+        holder.bind(listLatest[position])
     }
 }
-
 //займусь ими чуть позже, сначала надо реализовать фрагменты
